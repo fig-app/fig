c2aa847f737c7b5697f5f0c4e43d980cebe1e0c1
Instelce <instelce@protonmail.com>
2024-09-17 19:47:52 +0200

    feat: setup architecture

---
+++ app/src/routes/+layout.ts
@@ -0,0 +1,8 @@
+import type { LayoutLoad } from './$types';
+
+export const prerender = true;
+export const ssr = false;
+
+export const load = (async () => {
+    return {};
+}) satisfies LayoutLoad;
