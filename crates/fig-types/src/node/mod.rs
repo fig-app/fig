4f5973f077dce6a3736582fcc9d4dfa268c6983d
Instelce <instelce@protonmail.com>
2024-09-28 18:34:34 +0200

    feat(types): add global node, document, canvas and vector attributes

---
+++ crates/fig-types/src/node/mod.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+
+pub struct Node {
+    /// A string uniquely identifying this node within the document.
+    id: String,
+    /// The name given to the node by the user in the tool.
+    name: String,
+    /// Whether or not the node is visible on the canvas.
+    visible: bool,
+    /// The type of the node.
+    ntype: NodeType,
+    /// The rotation of the node, if not 0.
+    rotation: i32
+}
+
+pub enum NodeType {
+    Document(DocumentNode),
+    Canvas(CanvasNode),
+    Vector(VectorNode)
+}
+
+pub struct DocumentNode {
+    /// An array of canvases attached to the document.
+    children: Vec<Node>
+}
+
+pub struct CanvasNode {
+    /// An array of top level layers on the canvas
+    children: Vec<Node>,
+    //// Background color of the canvas.
+    // background_color: Color,
+    //// An array of export settings representing images to export from the canvas
+    // export_settings: Vec<ExportSetting>
+}
+
+pub struct VectorNode {
+    /// If true, layer is locked and cannot be edited.
+    locked: bool,
+    //// An array of export settings representing images to export from the node.
+    // export_settings: Vec<ExportSetting>,
+    //// How this node blends with nodes behind it in the scene
+    // blend_mode: BlendMode,
+    /// Keep height and width constrained to same ratio
+    preserve_ratio: bool,
+    // layout_align: LayoutAlign,
+    layout_grow: i32,
+    // constraint: LayoutConstraint,
+    opacity: f32,
+    // absolute_bounding_box: Rectangle,
+    // absolute_render_bounds: Option<Rectangle>,
+    // effects: Vec<Effect>,
+    // size: Vector or Size ?
+    // fills: Vec<Paint>,
+    // fill_geometry: Vec<Path>,
+    // strokes: Vec<Paint>,
+    stroke_weight: i32,
+    // individual_stroke_weights: StrokeWeights,
+    // stroke_cap: StrokeCap,
+    stroke_dashes: Vec<i32>,
+    // stroke_geometry: Vec<Path>,
+    // stroke_align: StrokeAlign,
+}
